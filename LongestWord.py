f578fbd8a2c0d1b50073697c920754eb91b594ba
neeil93 <chandrasunil93@gmail.com>
2018-04-15 16:15:14 +0530

    Longest Word
    Initial Commit

---
+++ LongestWord.py
@@ -0,0 +1,31 @@
+'''Implement a function longestWord() that takes a list of words and returns the longest
+one.'''
+
+#Define the function with parameter as a list of words
+def longestWord(wordList):
+    
+    #variable to store length of longest word
+    wordLen = 0
+    
+    #variable to store longest word
+    longestWord=wordList[1]
+    
+    #loop the list to find the largest word
+    for x in wordList:
+        
+        #check if current words length is greater than max word length
+        if len(x)>wordLen:
+            #assign max length to the new word length
+            wordLen=len(x)
+            
+            #assign the new longest word
+            longestWord=x
+            
+    #return the longest word
+    return longestWord
+    
+#Define list of words
+wordList = ["BarryAllen", "BruceWayne", "ElenaGilbert", "ClarkKent", "EmiliaClarke"]
+
+#Call the function and print the output
+print("The Longest word in the List is : "+longestWord(wordList))
